a73d213bc53b296027f45543666e79e842116452
Gairick Saha <gairicksaha@gmail.com>
2023-05-02 00:14:18 +0530

    default cellWidth changed from 100 to 100 and enableColumnWidthDrag, enableRowHeightDrag boolean var added

--- lib/src/models/flutter_spreadsheet_ui_config.dart
+++ lib/src/models/flutter_spreadsheet_ui_config.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 
 class FlutterSpreadsheetUIConfig {
   const FlutterSpreadsheetUIConfig({
-    this.cellWidth = 100,
+    this.cellWidth = 110,
     this.cellHeight = kMinInteractiveDimension,
     this.freezedColumnWidth,
     this.headerHeight,
     this.freezedColumnExtendedByWidth = 50,
     this.freezeFirstColumn = true,
     this.freezeFirstRow = true,
+    this.enableColumnWidthDrag = false,
+    this.enableRowHeightDrag = false,
   });
 
   final double cellWidth, cellHeight, freezedColumnExtendedByWidth;
   final double? freezedColumnWidth, headerHeight;
-  final bool freezeFirstColumn, freezeFirstRow;
+  final bool freezeFirstColumn,
+      freezeFirstRow,
+      enableColumnWidthDrag,
+      enableRowHeightDrag;
 }
